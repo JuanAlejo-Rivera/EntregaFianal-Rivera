8117e2223b324cf1341599a9ac16b16669d95018
PrimeraEntrega <juan.alejorivera25@gmail.com>
2023-01-25 08:52:56 -0500

    Actualizacion

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,16 @@
 import ItemListContainer from "./ItemListContainer"
+import { Route, Routes } from "react-router-dom"
 
 const Main = () => {
     return (
         <main>
-            <h2>Home</h2>
-            <ItemListContainer/>
+            <Routes>
+                {/* <h2>Home</h2>
+                <ItemListContainer /> */}
+                <Route path="/" element={<ItemListContainer/>}/>
+                <Route path="/Cervezas" element={<p>Cenvezas</p>}/>
+                <Route path="/Vinos" element={<p>Vinos</p>}/>
+            </Routes>
         </main>
     )
 }
